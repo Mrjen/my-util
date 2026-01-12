f54f8e2905479b00c830f634f567d4aaa16b1d08
chenlin <victor.chen@grayshark.com>
2026-01-12 15:56:47 +0800

    feat(vite): 集成 code-inspector-plugin 代码检查插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import { codeInspectorPlugin } from "code-inspector-plugin"
 import path from "path"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    codeInspectorPlugin({
+      bundler: "vite",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
